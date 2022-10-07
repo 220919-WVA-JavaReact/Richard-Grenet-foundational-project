735f6e6163fbe84715d4bb78c29ccac3edc4f9bd
Eokn <EoknDev@gmail.com>
2022-10-07 10:09:08 -0400

    tweaking a few comments/descriptions

--- src/main/java/com/revature/app/App.java
+++ src/main/java/com/revature/app/App.java
@@ -17,7 +17,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Reimbursement Ticket App!");
-        System.out.println("Enter 'exit' to exit the app at any time.");
+        System.out.println("Enter 'exit' to exit the app when in selection menus.");
         //scan for user input to decide what actions to take
         Scanner sc = new Scanner(System.in);
         Employee loggedInUser = null;
@@ -26,21 +26,18 @@ public class App {
         String menu = "LoginRegister";
         while(running){
 
-            //menu switch case
+            //menu switch case - menu reassigned after spin cycle is over to swap between menus. Relevant persistent data passed in and returned when applicable.
 
             switch (menu) {
                 case "LoginRegister":
-                    //call on method in menu to show text and do choices. maybe a method called 'spin' which just does the logic once and takes in the menu and scanner.
                     menu=LoginRegister.spin(menu, sc);
                     break;
                 case "Login":
-                    //call on method in menu to show text and do choices. maybe a method called 'spin' which just does the logic once and takes in the menu and scanner.
                     List loginResult = Login.spin(es,sc);
                     menu= loginResult.get(0).toString();
                     loggedInUser = (Employee) loginResult.get(1);
                     break;
                 case "Register":
-                    //call on method in menu to show text and do choices. maybe a method called 'spin' which just does the logic once and takes in the menu and scanner.
                     List registerResult = Register.spin(es, sc);
                     menu= registerResult.get(0).toString();
                     loggedInUser = (Employee) registerResult.get(1);
